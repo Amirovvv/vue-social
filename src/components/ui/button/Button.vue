7ad726bf893396fb07d6be42bd784b4f71ce199a
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2022-12-21 02:06:44 +0300

    ui: added button

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import buttonProps from './ButtonTypes'
+
+const props = defineProps(buttonProps())
+
+const btnType = computed(() => `${props.type}Btn`)
+</script>
+
+<template>
+  <button :class="$style[btnType]"><slot></slot></button>
+</template>
+
+<style module>
+@import './index.module.scss';
+</style>
